ab76330e0e6188949160a10e88817aef8832a9b3
ryanroque17 <ryanroque17@gmail.com>
2017-11-05 03:32:51 +0800

    No more bugs

--- index.js
+++ index.js
@@ -1,15 +1,57 @@
 function drag() {
-    $(".draggable").draggable({cancel:false});
+    $(".element").draggable({cancel:false});
     
   };
 
+
+$(document).on("click", ".element", function(){
+
+  	var prop = document.getElementById("properties");
+   	prop.style.display = 'block';
+    prop.innerHTML=
+    "Properties: <label>Height=</label> <input id='heightprop' style='width:45px' type='text' value='" + $(this).height() +"px' </input> "+
+    "<label>Width=</label> <input id='widthprop' style='width:45px' type='text' value='" + $(this).width() +"px'> </input>" +
+    "<label>Placeholder =</label> <input id='placeholderprop' style='width:75px' type='text' >"+
+    "<button id='changeprop' style='width:105px'> Save Changes </button>";
+
+    eval(changeprop($(this)));
+});
+
+$(document).on("click", ".label", function(){
+
+  	var prop = document.getElementById("properties");
+   	prop.style.display = 'block';
+    prop.innerHTML=
+    "Properties: <label>Font size =</label> <input id='fontsizeprop' style='width:45px' type='text' value='" + $(this).css('font-size') +"' </input> "+
+    "<label>Text =</label> <input id='textprop' style='width:75px' type='text' value='"+ $(this).text() + "'> </input>" +
+    "<button id='changeprop' style='width:105px'> Save Changes </button>";
+
+    eval(changeprop($(this)));
+});
+
+function changeprop(element) {
+	document.getElementById("changeprop").addEventListener("click", function(e) { 
+		if(element.attr('class').indexOf("label") >= 0){
+			element.css({'font-size': document.getElementById("fontsizeprop").value})
+			element.text(document.getElementById("textprop").value);
+		}else{
+			element.height(document.getElementById("heightprop").value) ;
+			element.width(document.getElementById("widthprop").value);
+			if(element.attr('class').indexOf("input") >= 0)
+				element.attr("placeholder", document.getElementById("placeholderprop").value);
+			else
+				element.val(document.getElementById("placeholderprop").value);
+
+		}
+	})
+}
 document.getElementById("addlabel").addEventListener("click", function(e) { 
 	
 	e.preventDefault();
     console.log(e.pageX);
     console.log(e.pageY);
 
-	var html = document.getElementById("canvas").innerHTML+="<label class='resizable draggable label' style='position:absolute; top: "+ e.pageX + ";left: "+ e.pageY+"'>hi</label>";
+	var html = document.getElementById("canvas").innerHTML+="<label class='element label' style='position:absolute; top: "+ e.pageY + ";left: "+ e.pageX+"'>Label</label>";
 
 /*	var ihtml = document.getElementById("canvas").innerHTML;
 	var ohtml = document.getElementById("canvas").outerHTML;*/
@@ -17,6 +59,7 @@ document.getElementById("addlabel").addEventListener("click", function(e) {
 	
 
 	eval(drag());
+	console.log(html);
 /*    console.log(ihtml);
 	console.log(ohtml);*/
 })
@@ -26,7 +69,7 @@ document.getElementById("addtextbox").addEventListener("click", function(e) {
     console.log(e.pageX);
     console.log(e.pageY);
 
-	var html = document.getElementById("canvas").innerHTML+="<input type='text' class='resizable draggable label' style='position:absolute; top: "+ e.pageX + ";left: "+ e.pageY+"'></input>";
+	var html = document.getElementById("canvas").innerHTML+="<input type='text' class='element input' style='position:absolute; top: "+ e.pageY + ";left: "+ e.pageX+"'></input>";
 
 /*	var ihtml = document.getElementById("canvas").innerHTML;
 	var ohtml = document.getElementById("canvas").outerHTML;*/
@@ -43,7 +86,7 @@ document.getElementById("addbutton").addEventListener("click", function(e) {
     console.log(e.pageX);
     console.log(e.pageY);
 
-	var html = document.getElementById("canvas").innerHTML+="<button class='resizable draggable label' style='position:absolute; top: "+ e.pageX + ";left: "+ e.pageY+"'>Button</button>";
+	var html = document.getElementById("canvas").innerHTML+="<input type='submit' class='element button' style='position:absolute; top: "+ e.pageY + ";left: "+ e.pageX+"'></input>";
 
 /*	var ihtml = document.getElementById("canvas").innerHTML;
 	var ohtml = document.getElementById("canvas").outerHTML;*/
@@ -60,7 +103,7 @@ document.getElementById("generatejson").addEventListener("click", function() {
 
     var data = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(json));
 	var downloadElement = document.getElementById('downloadJsonFile');
-	downloadElement.setAttribute("href",     dataStr     );
+	downloadElement.setAttribute("href",     data     );
 	downloadElement.setAttribute("download", "ui_design.json");
 	downloadElement.click();
 
